bc9ca2dc46dfebd847b2575d3117db0738cc739a
Niki Khanezai <nikikhanezai@gmail.com>
2026-08-14 15:54:09 +0100

    Create grade_analyzer.py

---
+++ scripts/grade_analyzer.py
@@ -0,0 +1,23 @@
+students = {}
+
+while True:
+
+    name = input("Enter name: ")
+
+    if name == "done" : break
+
+    grade = input("Enter grade: ")
+
+    students[name] = float(grade)
+
+
+
+highest = max(students.values())
+lowest = min(students.values())
+average = round(sum(students.values()) / len(students), 2)
+
+for name, grade in students.items():
+    print(f"{name}: {grade}")
+print(f"Highest: {highest}")
+print(f"Lowest: {lowest}")
+print(f"Average: {average}")
