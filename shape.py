462b670ec5728af8a9f4ce23cf67ca5efd9a277d
Niki Khanezai <nikikhanezai@gmail.com>
2026-08-28 13:42:24 +0100

    Add shape class project

---
+++ shape.py
@@ -0,0 +1,23 @@
+class Shape:
+    def area(self):
+        return 0
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+c = Circle(5)
+r = Rectangle(4, 6)
+print(c.area())
+print(r.area())
