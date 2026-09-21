193644210e446b17ad974078653eca111eb1ed22
Niki Khanezai <nikikhanezai@gmail.com>
2026-09-21 14:25:51 +0100

    Add my_package with maths and strings modules

---
+++ my_package/maths.py
@@ -0,0 +1,7 @@
+def multiply(a: int, b: int) -> int:
+    """ Returns the multiplied result """
+    return a * b
+
+def add(a: int, b: int) -> int:
+    """Returns the addition"""
+    return a + b
---
+++ my_package/strings.py
@@ -0,0 +1,9 @@
+def reverse(s: str) -> str:
+    """Reverses a string"""
+    return s[::-1]
+
+def uppercase(s: str) -> str:
+    """Returns a string as uppercase"""
+    return s.upper()
+
+
