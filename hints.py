035e5fb32a5539b2a307b3e3e44e83e846fcc25d
Niki Khanezai <nikikhanezai@gmail.com>
2026-09-21 14:08:24 +0100

    Expaning type hints and docstrings

--- hints.py
+++ hints.py
@@ -4,5 +4,17 @@ def multiply(a: int, b: int) -> int:
 def get_initials(name: str) -> str:
     return name[0].upper()
 
-print(multiply(3, 8))
-print(get_initials("niki"))
+#print(multiply(3, 8))
+#print(get_initials())
+
+def add(a: int, b: int) -> int:
+    """Add two numbers and return the result."""
+    return a + b
+
+print(add(2, 4))
+
+def calculate_bmi(weight_kg: float, height_m: float) -> float:
+    """Returns the BMI"""
+    return round(weight_kg / (height_m * height_m), 2)
+
+print(calculate_bmi(55.5, 1.58))
